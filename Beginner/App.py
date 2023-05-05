0dedb0c5428ca3535af52eb03c25ccdc0fccb2c0
t2haev00 <eveliinahampus@gmail.com>
2023-05-05 18:04:52 +0300

    Printing and variables.

---
+++ Beginner/App.py
@@ -0,0 +1,10 @@
+# Printing
+print("Hello World")
+print("!")
+
+
+# Variables
+character_name = "John"
+character_age = "35"
+
+print("Character name is " + character_name + " and its age is " + character_age)
